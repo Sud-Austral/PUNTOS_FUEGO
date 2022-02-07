9b13920895256e0b034baa4189c63a8c80db9aa8
lmonsalve22 <lmonsalve22@gmail.com>
2022-02-07 11:53:53 -0300

    Update automatizacion.py

--- automatizacion.py
+++ automatizacion.py
@@ -8,11 +8,7 @@ import pandas as pd
 from geopy.geocoders import Nominatim
 
 fuentes = [["https://firms.modaps.eosdis.nasa.gov/data/active_fire/modis-c6.1/csv/MODIS_C6_1_South_America_7d.csv",
-            "MODIS"],
-            ["https://firms.modaps.eosdis.nasa.gov/data/active_fire/suomi-npp-viirs-c2/csv/SUOMI_VIIRS_C2_South_America_7d.csv",
-            "SUOMI"],
-            ["https://firms.modaps.eosdis.nasa.gov/data/active_fire/noaa-20-viirs-c2/csv/J1_VIIRS_C2_South_America_7d.csv",
-            "J1"]]
+            "MODIS"]]
 
 def setComuna(x):
     if(x["city"] != ""):
@@ -69,20 +65,6 @@ def descarga(fuente):
     dfLat = dfDate[dfDate["latitude"] < -16.5]
     dfLat2 = dfLat[dfLat["longitude"] < -69.5]
     dfLat2 = dfLat2.reset_index()
-    try:
-        dfLat2 = getComunas(dfLat2)
-        dfLat2.to_csv(f"Data/{fuente[1]}/Puntos_Diarios_{fuente[1]}.csv",encoding='utf-8-sig')
-        dfLat2.to_csv(f"Data_Legacy/{fuente[1]}/Puntos_Diarios_{fuente[1]}_{datetime.datetime.now().strftime('%Y-%m-%d')}.csv",encoding='utf-8-sig')    
-    except:
-        error = sys.exc_info()[1]
-        print(error)
-        print("Error en getComunas")
-    
-    # AQUÍ SE PODRÍA AGREGAR LA INFORMACIÓN CALLE, COMUNA, PROVINCIA, REGIÓN.
-    # CALLE, COMUNA, PROVINCIA, REGIÓN (INCLUIR JSON)
-
-    
-    
     return dfLat2
 
 def getJSON(fuente):
@@ -119,30 +101,27 @@ def proceso():
     return
 
 def saveConsolidado():
-    consolidado = pd.read_excel("Consolidado/ConsolidadoPuntosFuego.xlsx")
-    salida2 = []
-    for i in fuentes:
-        dfaux = pd.read_csv(i[0])
-        dfaux["Fuente"] = i[1]
-        salida2.append(dfaux)
+    data = pd.read_excel(r"Data\ConsolidadoPuntosFuego.xlsx")
 
-    consolidadoUpdate = pd.concat(salida2)
-    consolidadoUpdate = consolidadoUpdate[consolidadoUpdate["acq_date"].apply(lambda x: x not in consolidado["acq_date"].unique())]
+
+    df           = pd.read_csv(fuentes[0])
+    df["Fuente"] = fuentes[1]
+    consolidadoUpdate = df[df["acq_date"].apply(lambda x: x not in data["Fecha_Texto"].unique())]
     #consolidadoUpdate = consolidadoUpdate.reset_index()
     dfDate = consolidadoUpdate
     dfLat = dfDate[dfDate["latitude"] < -16.5]
     dfLat2 = dfLat[dfLat["longitude"] < -69.5]
     dfLat2 = dfLat2.reset_index()
-    try:
-        dfLat2 = getComunas(dfLat2)
-    except:
-        error = sys.exc_info()[1]
-        print(error)
-        print("Error en getComunas")
-    dfFinal = pd.concat([consolidado,dfLat2])
+    #try:
+    #    dfLat2 = getComunas(dfLat2)
+    #except:
+    #    error = sys.exc_info()[1]
+    #    print(error)
+    #    print("Error en getComunas")
+    #dfFinal = pd.concat([consolidado,dfLat2])
     #dfFinal = dfFinal.reset_index()
     #dfFinal2 = dfFinal.drop(columns=["level_0","index","Unnamed: 0"])
-    dfFinal.to_excel("Consolidado/ConsolidadoPuntosFuego.xlsx", index=False)
+    dfLat2.to_excel("Test.xlsx", index=False)
     print("ya deberia estar")
     #print(dfFinal)
     return 
@@ -157,40 +136,4 @@ if __name__ == '__main__':
     except:
         error = sys.exc_info()[1]
         print(error)
-        #try:
-            #proceso()
-        #    print("Hola todo bien")
-        #except:
-        #    error = sys.exc_info()[1]
-        #    print(error)
-    
-    #geolocator = Nominatim(user_agent="geoapiExercises")
-    #for i in fuentes:
-    #    df = pd.read_csv(i[0])
-    #    dfDate = df
-    #    dfLat = dfDate[dfDate["latitude"] < -16.5]
-    #    dfLat2 = dfLat[dfLat["longitude"] < -69.5]
-    #    dfLat2 = dfLat2.reset_index()
-    #    dfLat2 = getComunas(dfLat2)
-        #dfLat2['Coordenadas'] = dfLat2[['latitude', 'longitude']].apply(lambda x: f'{x.latitude},{x.longitude}', axis=1)
-        #dfLat2["Locacion"] = dfLat2["Coordenadas"].apply(lambda x: geolocator.reverse(x))
-        #referencia = ['road', 'city', 'county', 'state', 'country', 'country_code',
-        #    'industrial', 'town', 'isolated_dwelling', 'hamlet', 'postcode',
-        #    'building', 'neighbourhood', 'region', 'suburb', 'amenity',
-        #    'man_made', 'village', 'office', 'historic', 'aeroway', 'tourism',
-        #    'state_district', 'highway']
-        #for i in referencia:
-        #    def AgregarColumn(x):
-        #        try:
-        #            return x.raw["address"][i]
-        #        except:
-        #            return ""
-        #    df[i] = df["Locacion"].apply(lambda x: AgregarColumn(x))
-        #dfChile = df[df["country"] == "Chile"]
-        #dfChile = dfChile.reset_index()
-        #dfChile["Comuna"] = dfChile[["city","town","village","suburb"]].apply(setComuna, axis=1)
-        #ref = pd.read_excel(r"LocalizaGoogle.xlsx")
-        #ref = ref[['REGION', 'PROVINCIA', 'COMUNA','Comuna', 'ComunaUpper', 'raw']]
-        #dfFinal = dfChile.merge(ref, left_on='Comuna', right_on='Comuna',how="left")
-        #print(dfLat2)
     
